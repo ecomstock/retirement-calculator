06508e688707e12372833e5dccf7021ef6b923c8
Eric <ecomstock@gmail.com>
2018-02-15 21:33:40 -0800

    Fix left margin of income helper text.

--- src/User.js
+++ src/User.js
@@ -1,72 +1,103 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import TextField from 'material-ui/TextField';
+import Input, { InputLabel } from 'material-ui/Input';
+import { FormControl, FormControlLabel, FormHelperText } from 'material-ui/Form';
 
 const User = props =>
 	<div>
   	<h3 className="left-margin">Tell us about yourself</h3>
-		<TextField
-			className="left-margin"
-			label="Age"
-			margin="normal"
-		/>
+		<FormControl>
+			<InputLabel className="left-margin">Age</InputLabel>
+			<Input
+				className="left-margin"
+				margin="normal"
+			/>
+		</FormControl>
 		<i class="material-icons">help_outline</i>
 		<br />
-		<TextField
-			label="Average total monthly income after taxes. Do not include spouse's income."
-			margin="normal"
-		/>
+		<FormControl>
+			<InputLabel className="left-margin">Your total monthly income after taxes</InputLabel>
+			<Input
+				className="left-margin"
+				margin="normal"	
+			/>
+			<FormHelperText id="fh-left-margin">Do not include spouse's income</FormHelperText>
+		</FormControl>
 		<i class="material-icons">help_outline</i>
 		<br />
-		<p>Will you be eligible for Social Security benefits?</p>
-		<p>Do you have a defined benefit pension?</p>
-		<h3>Your accounts</h3>
-		<p>401(k)/403(b)</p>
-		<TextField
-			label="Account balance"
-			margin="normal"
-		/>
+		<p className="left-margin">Will you be eligible for Social Security benefits?</p>
+		<p className="left-margin">Do you have a defined benefit pension?</p>
+		<h3 className="left-margin">Your accounts</h3>
+		<p className="left-margin">401(k)/403(b)</p>
+		<FormControl>
+			<InputLabel className="left-margin">Account balance</InputLabel>
+			<Input
+				className="left-margin"
+				margin="normal"
+			/>
+		</FormControl>
 		<i class="material-icons">help_outline</i>
-		<TextField
-			label="Annual contribution"
-			margin="normal"
-		/>
-		<i class="material-icons">help_outline</i>
-		<br />
-		<p>IRA</p>
-		<TextField
-			label="Account balance"
-			margin="normal"
-		/>
-		<i class="material-icons">help_outline</i>
-		<TextField
-			label="Annual contribution"
-			margin="normal"
-		/>
+		<FormControl>
+			<InputLabel className="left-margin">Annual contribution</InputLabel>
+			<Input
+				className="left-margin"
+				margin="normal"
+			/>
+		</FormControl>
 		<i class="material-icons">help_outline</i>
 		<br />
-		<p>Roth IRA</p>
-		<TextField
-			label="Account balance"
-			margin="normal"
-		/>
+		<p className="left-margin">IRA</p>
+		<FormControl>
+			<InputLabel className="left-margin">Account balance</InputLabel>
+			<Input
+				className="left-margin"
+				margin="normal"
+			/>
+		</FormControl>
 		<i class="material-icons">help_outline</i>
-		<TextField
-			label="Annual contribution"
-			margin="normal"
-		/>
+		<FormControl>
+			<InputLabel className="left-margin">Annual contribution</InputLabel>
+			<Input
+				className="left-margin"
+				margin="normal"
+			/>
+		</FormControl>
 		<i class="material-icons">help_outline</i>
 		<br />
-		<p>Private accounts (stocks, bonds, mutual funds, ETFs, etc.)</p>
-		<TextField
-			label="Account balance"
-			margin="normal"
-		/>
+		<p className="left-margin">Roth IRA</p>
+		<FormControl>
+			<InputLabel className="left-margin">Account balance</InputLabel>
+			<Input
+				className="left-margin"
+				margin="normal"
+			/>
+		</FormControl>
 		<i class="material-icons">help_outline</i>
-		<TextField
-			label="Annual contribution"
-			margin="normal"
-		/>
+		<FormControl>
+			<InputLabel className="left-margin">Annual contribution</InputLabel>
+			<Input
+				className="left-margin"
+				margin="normal"
+			/>
+		</FormControl>
+		<i class="material-icons">help_outline</i>
+		<br />
+		<p className="left-margin">Private accounts (stocks, bonds, mutual funds, ETFs, etc.)</p>
+		<FormControl>
+			<InputLabel className="left-margin">Account balance</InputLabel>
+			<Input
+				className="left-margin"
+				margin="normal"
+			/>
+		</FormControl>
+		<i class="material-icons">help_outline</i>
+		<FormControl>
+			<InputLabel className="left-margin">Annual contribution</InputLabel>
+			<Input
+				className="left-margin"
+				margin="normal"
+			/>
+		</FormControl>
 		<i class="material-icons">help_outline</i>
 		<br />
 	</div>
